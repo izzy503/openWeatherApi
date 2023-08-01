6a7206334aab55d450d18de3b3c02755123db2c6
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-07-31 18:22:06 -0700

    this will be first comment

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Character from "./js/index.js";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+window.addEventListener("load", function () {
+  console.log("openWeatherApi")
+));
+
+
+
